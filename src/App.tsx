a499e0d1c18b74e484c4ca45fac6575511b5d2a0
Bogdan Gordeychuk <comedi4@gmail.com>
2022-04-25 12:42:35 +0600

    конец? надеюсь

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,101 @@ import "./index.css";
 
 function App() {
   //localStorage.clear();
-  const [isGreeting, setIsGreeting] = useState(false);
-  const data: { id: number; name: string }[] = [
+  const name: string | number | null = localStorage.getItem("name");
+  const [isGreeting, setIsGreeting] = useState(Boolean(name));
+
+  const [data, setData] = useState([
     { id: 0, name: "TODO" },
     { id: 1, name: "In Progress" },
     { id: 2, name: "Testing" },
     { id: 3, name: "Done" },
-  ];
+  ]);
+
+  const [cards, setCards] = useState<
+    {
+      name?: string;
+      cardId?: number;
+      columnId?: number;
+    }[]
+  >([]);
+
+  const [description, setDescription] = useState<
+    {
+      name?: string;
+      cardId?: number | undefined | null;
+      columnId?: number | null | undefined;
+    }[]
+  >([]);
+
+  const [comments, setComments] = useState<
+    {
+      name?: string;
+      cardId?: number | undefined | null;
+      columnId?: number | null | undefined;
+      commentId?: number | null | undefined;
+    }[]
+  >([]);
 
   useEffect(() => {
-    localStorage.setItem("columns", JSON.stringify(data));
+    if (localStorage.getItem("columns") === null) {
+      localStorage.setItem("columns", JSON.stringify(data));
+    } else if (JSON.parse(localStorage.getItem("columns")!) !== data) {
+      setData(JSON.parse(localStorage.getItem("columns")!));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  useEffect(() => {
+    if (localStorage.getItem("cards") === null) {
+      localStorage.setItem("cards", JSON.stringify(cards));
+    } else if (JSON.parse(localStorage.getItem("cards")!) !== cards) {
+      setCards(JSON.parse(localStorage.getItem("cards")!));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("description") === null) {
+      localStorage.setItem("description", JSON.stringify(description));
+    } else if (
+      JSON.parse(localStorage.getItem("description")!) !== description
+    ) {
+      setDescription(JSON.parse(localStorage.getItem("description")!));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("comments") === null) {
+      localStorage.setItem("comments", JSON.stringify(comments));
+    } else if (JSON.parse(localStorage.getItem("comments")!) !== comments) {
+      setComments(JSON.parse(localStorage.getItem("comments")!));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
     localStorage.getItem("name") === null
       ? localStorage.setItem("name", "")
       : localStorage.getItem("name");
   }, []);
 
-  const name: string = localStorage.getItem("name")!;
-
   return (
     <div className="App">
-      {!isGreeting && name === "" ? (
+      {!isGreeting ? (
         <GreetingPopup closeWindow={() => setIsGreeting(true)} />
       ) : (
-        <Board name={name} data={data} />
+        <Board
+          name={name}
+          data={data}
+          setData={setData}
+          cards={cards}
+          setCards={setCards}
+          description={description}
+          setDescription={setDescription}
+          comments={comments}
+          setComments={setComments}
+        />
       )}
     </div>
   );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,18 +3,92 @@ import styled from "styled-components";
 import Column from "./Column";
 
 type Name = {
-  name: string | number;
+  name: string | number | null;
   data: { id: number; name: string }[];
+  cards: {
+    name?: string;
+    cardId?: number;
+    columnId?: number;
+  }[];
+  description: {
+    name?: string;
+    cardId?: number | undefined | null;
+    columnId?: number | null | undefined;
+  }[];
+  comments: {
+    name?: string;
+    cardId?: number | undefined | null;
+    columnId?: number | null | undefined;
+    commentId?: number | null | undefined;
+  }[];
+
+  setData: React.Dispatch<
+    React.SetStateAction<
+      {
+        id: number;
+        name: string;
+      }[]
+    >
+  >;
+  setCards: React.Dispatch<
+    React.SetStateAction<
+      {
+        name?: string;
+        cardId?: number;
+        columnId?: number;
+      }[]
+    >
+  >;
+  setDescription: React.Dispatch<
+    React.SetStateAction<
+      {
+        name?: string;
+        cardId?: number | undefined | null;
+        columnId?: number | null | undefined;
+      }[]
+    >
+  >;
+
+  setComments: React.Dispatch<
+    React.SetStateAction<
+      {
+        name?: string;
+        cardId?: number | undefined | null;
+        columnId?: number | null | undefined;
+        commentId?: number | null | undefined;
+      }[]
+    >
+  >;
 };
 
-const Board: React.FC<Name> = ({ name, data }) => {
+const Board: React.FC<Name> = ({
+  name,
+  data,
+  setData,
+  cards,
+  setCards,
+  description,
+  setDescription,
+  comments,
+  setComments,
+}) => {
   return (
     <>
       <Header>
         <p>Привет, {name}</p>
       </Header>
       <Content>
-        <Column data={data} />
+        <Column
+          data={data}
+          setData={setData}
+          name={name}
+          cards={cards}
+          setCards={setCards}
+          description={description}
+          setDescription={setDescription}
+          comments={comments}
+          setComments={setComments}
+        />
       </Content>
     </>
   );
