354020b7a7cea6f94ccb3e4f1cf0dc4001b21439
Bogdan Gordeychuk <comedi4@gmail.com>
2022-05-12 20:17:00 +0600

    feat: application initial markup

---
+++ UI/header/HeaderLayout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const HeaderLayout = ({ children }: Props) => {
+  return <Header>{children}</Header>;
+};
+
+export default HeaderLayout;
+
+const Header = styled.div`
+  width: 1270px;
+  height: 100px;
+  margin: 32px auto;
+`;
